8a826cb88e08afc18ea91084c5cf71791da1b11b
Aleksandr <77121018+gitaleksksks@users.noreply.github.com>
2024-12-24 14:37:20 +0300

    Add files via upload

---
+++ src/com/kabanchiki/algorithms/BooksSort.java
@@ -0,0 +1,21 @@
+package algorithms.sorting;
+
+public class BooksSort implements UniversalMergeSort {
+
+    public void sortAuthor() {
+
+    }
+
+    public void sortName() {
+
+    }
+
+    public void sortPages() {
+
+    }
+
+    @Override
+    public <T> T mergeSort(T[] array) {
+        return null;
+    }
+}
