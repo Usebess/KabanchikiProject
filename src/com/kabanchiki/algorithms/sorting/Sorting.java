6e5eb8cde472ab97e5afb92e9c8b95f5f142a265
Aleksandr <77121018+gitaleksksks@users.noreply.github.com>
2024-12-25 17:44:49 +0300

    Sorting.java

--- src/com/kabanchiki/algorithms/sorting/Sorting.java
+++ src/com/kabanchiki/algorithms/sorting/Sorting.java
@@ -3,7 +3,7 @@ package algorithms.sorting;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Sorting<T extends Comparable<T>> {
+public class Sorting<T extends Comparable<T>> implements UniversalMergeSort<T> {
 
     UniversalMergeSort<T> universalMergeSort;
 
